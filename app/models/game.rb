4d5d929d22727ca1d9212b8636f3a701773f9dd7
jayastronomic <juliansmith94@hotmail.com>
2025-08-13 19:35:31 -0500

    Add validations to models

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,4 +1,9 @@
 class Game < ApplicationRecord
   belongs_to :user
   has_many :guesses
+
+  validates :solution,
+            presence: true,
+            length: { is: 4 },
+            format: { with: /\A[0-7]{4}\z/, message: "must be exactly 4 digits between 0 and 7" }
 end
--- app/models/guess.rb
+++ app/models/guess.rb
@@ -1,3 +1,9 @@
 class Guess < ApplicationRecord
   belongs_to :game
+
+  validates :report, presence: true
+  validates :value,
+            presence: true,
+            length: { is: 4 },
+            format: { with: /\A[0-7]{4}\z/, message: "must be exactly 4 digits between 0 and 7" }
 end
