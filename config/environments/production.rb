9704480f6c5e4993f296420bf2967c59d87295a4
jayastronomic <juliansmith94@hotmail.com>
2025-08-15 05:52:37 -0500

    add railway host to config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,6 +3,8 @@ require "active_support/core_ext/integer/time"
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  # Allow Railway host
+  config.hosts << "mastermind-api-production.up.railway.app"
   # Code is not reloaded between requests.
   config.enable_reloading = false
 
