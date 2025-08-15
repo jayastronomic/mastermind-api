a9836f2337c838336fa1c563e033916fc9b02fce
jayastronomic <juliansmith94@hotmail.com>
2025-08-15 06:25:16 -0500

    add railway host to prod and dev  config

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -2,7 +2,7 @@ require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-
+  config.hosts << "mastermind-api-production.up.railway.app"
   # Make code changes take effect immediately without server restart.
   config.enable_reloading = true
 
