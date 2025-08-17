925edf94e95523891fd75615954434a27642f52f
jayastronomic <juliansmith94@hotmail.com>
2025-08-17 02:47:47 -0500

    remove cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -5,13 +5,13 @@
 
 # Read more: https://github.com/cyu/rack-cors
 
-Rails.application.config.middleware.insert_before 0, Rack::Cors do
-  allow do
-    origins "http://localhost:5173"
+# Rails.application.config.middleware.insert_before 0, Rack::Cors do
+#   allow do
+#     origins "http://localhost:5173"
 
-    resource "*",
-      headers: :any,
-      methods: [ :get, :post, :put, :patch, :delete, :options, :head ],
-      credentials: true
-  end
-end
+#     resource "*",
+#       headers: :any,
+#       methods: [ :get, :post, :put, :patch, :delete, :options, :head ],
+#       credentials: true
+#   end
+# end
